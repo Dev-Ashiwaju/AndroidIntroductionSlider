e73b71e444bb82c7654d20dc6c25c9b49136decf
Dev-Ashiwaju <olizsurelove@gmail.com>
2021-01-20 10:51:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/introslider/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.introslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replay(v);
+            }
+        });
+    }
+
+    public void replay(View view) {
+        PreferenceManager preferenceManager = new PreferenceManager(getApplicationContext());
+        preferenceManager.setFirstTimeLaunch(true);
+        startActivity(new Intent(MainActivity.this, MainScreenActivity.class));
+        finish();
+    }
+}
